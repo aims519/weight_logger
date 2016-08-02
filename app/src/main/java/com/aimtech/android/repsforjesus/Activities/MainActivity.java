0eee01ede0bafad5148c5d6c2c4885ff243aabcb
Andrew Savory <andysav519@gmail.com>
2016-08-02 16:43:21 +0100

    Change parent theme

--- app/src/main/java/com/aimtech/android/repsforjesus/Activities/MainActivity.java
+++ app/src/main/java/com/aimtech/android/repsforjesus/Activities/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements DataBaseResetDial
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
-        getSupportActionBar().setTitle("  Weight Log");
+        getSupportActionBar().setTitle("   Weight Log");
 
         // Load default preferences (Once only when the app is first installed)
         PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
